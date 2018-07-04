50017ec9c2b183a9a441b7fb9f945b25bca5df08
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-04 13:57:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,16 @@
 # Add  code here!
 
 def prime?(int)
-  if int>1 && int 
+  for i in 2..(int-1)
+    if (int % i) == 0
+      return false
+    end
 end
+
+
+ def is_prime n
+  for d in 2..(n - 1)
+   if (n % d) == 0
+    return false
+   end
+  end
